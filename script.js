b13e55fc12fc190313ccadd924575d385ad88802
applesaucebld <maxim.puerstinger@gmx.at>
2024-11-27 22:15:37 +0100

    removed consolelog

--- script.js
+++ script.js
@@ -273,7 +273,6 @@ function openPtSettings() {
   // save buffers for comparison
   tempBufferArr = [];
   document.querySelector(".buffer").classList.add("selected");
-  console.log("testing", + document.querySelector(".buffer").classList.contains("selected"));
 
   function clearSelection() {
     const selected = container.querySelector(".selected");
